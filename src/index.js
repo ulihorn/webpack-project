9a765ed004f689383d979a1dd16d7357d34470bd
Uli Horn <ulihorn@gmail.com>
2021-02-22 16:50:36 +0100

    Add webpack.config.js; Add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
     refinedMoonstone: 4,
 };
 
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 5,
+};
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
