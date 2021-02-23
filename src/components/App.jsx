acd6db5d87f1a9efdafc931450433bfc6869dc5d
Uli Horn <ulihorn@gmail.com>
2021-02-23 18:49:08 +0100

    Add react refresh for development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,23 @@ import "../styles/index.scss";
 import Recipes from "./Recipes";
 import computer from "../images/computer.png";
 import lilly from "../images/lilly.svg";
+import Whatever from "./Whatever";
 
 //import React from 'react'
 
 const App = () => {
     return (
         <>
-         <section className="hero"></section>  
-         <main>
-             <section>
-                 <h1>Oh Hai, React</h1>
-             </section>
-        <img src={computer} alt="computer" width="250" />
-        <img src={lilly} alt="lilly" width="250" />
-        <Recipes /> 
-         </main>
+            <section className="hero"></section>  
+            <main>
+                <section>
+                    <h1>Oh Hai, React</h1>
+                </section>
+                <img src={computer} alt="computer" width="250" />
+                <img src={lilly} alt="lilly" width="250" />
+                <Recipes /> 
+                <Whatever />
+            </main>
         </>
     );
 };
---
+++ src/components/Whatever.jsx
@@ -0,0 +1,14 @@
+// @refresh reset
+import { useState } from "react";
+
+const Whatever = () => {
+    const [state, setState] = useState("Initial state")
+    return (
+        <div>
+            <h1>{state}</h1>
+            <button onClick={() => setState("Button clicked")}>Click me</button>
+        </div>
+    );
+};
+
+export default Whatever;
